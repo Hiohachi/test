40989b3341b209fa99a74aa470a95f548bfd41a0
Hiohachi <dbar@bk.ru>
2023-04-21 21:53:22 +0300

    первое дз

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int a = 0;
+int b = 0;
+
+System.Console.WriteLine("Введите первое число: ");
+a = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+b = int.Parse(Console.ReadLine());      //Ввод начальных чисел
+
+if ( a>b )
+{
+    System.Console.WriteLine($"Число {a} большее, а {b} меньшее.");
+}
+else if ( a<b )
+{
+    System.Console.WriteLine($"Число {b} большее, а {a} меньшее.");
+}
+else
+{
+    System.Console.WriteLine("Введённые числа равны.");
+}
